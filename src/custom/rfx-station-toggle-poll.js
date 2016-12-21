79fdc88adf655c78c713ae6690b6c328e5eae663
Aleksandr Sorokin <imarkoffko@gmail.com>
2016-12-22 01:34:46 +0300

    fix: rfx hook (#10)

--- src/custom/rfx-station-toggle-poll.js
+++ src/custom/rfx-station-toggle-poll.js
@@ -1,16 +1,44 @@
+const Promise = require('bluebird');
+
+function getOwners(poll) {
+  const { amqp, config } = this;
+  const { users: { audience, prefix, postfix, timeouts } } = config;
+  const route = `${prefix}.${postfix.list}`;
+  const timeout = timeouts.list;
+  const filter = {
+    stationChatId: {
+      eq: JSON.stringify(poll.get('ownerId')),
+    },
+    roles: {
+      match: 'admin',
+    },
+  };
+
+  return amqp.publishAndWait(route, { audience, filter }, { timeout });
+}
+
 function update(hasActivePoll = false, poll) {
   const { amqp, config } = this;
   const { users: { audience, prefix, postfix, timeouts } } = config;
   const route = `${prefix}.${postfix.updateMetadata}`;
-  const timeout = timeouts.verify;
-  const username = poll.get('ownerId');
+  const timeout = timeouts.updateMetadata;
   const metadata = {
     $set: {
       hasActivePoll,
     },
   };
 
-  return amqp.publishAndWait(route, { audience, username, metadata }, { timeout });
+  return Promise
+    .bind(this, poll)
+    .then(getOwners)
+    .get('users')
+    .map(user =>
+      amqp.publishAndWait(route, {
+        audience,
+        username: user.id,
+        metadata,
+      }, { timeout })
+    );
 }
 
 update.onStart = function onStart(poll) {
