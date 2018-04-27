8074df480da0e25ca26d078ef7aeb2923cbfe473
semaphore <semaphore@makeomatic.co>
2018-04-27 00:49:43 +0000

    docs(38): updated remote public documentation

---
+++ api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "",
+  "version": "0.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-04-27T00:49:42.720Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
